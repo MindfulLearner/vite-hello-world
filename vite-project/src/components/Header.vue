840224230d2dde19cf2aa90b022483983dff11de
MindfulLearner <dimajosh1009@gmail.com>
2024-09-10 23:48:28 +0000

    swaghell

---
+++ vite-project/src/components/Header.vue
@@ -0,0 +1,23 @@
+<script>
+    export default {
+        data() {
+            return {
+                message: 'hello mondo'
+            }
+        }
+    }
+</script>
+
+<template>
+    <div>
+        <h1>
+            {{ message }}
+        </h1>
+    </div>
+</template>
+
+<style scoped>
+    * {
+        color: lightblue;
+    }
+</style>
