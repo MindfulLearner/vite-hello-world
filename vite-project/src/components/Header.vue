d838c6773bab158142809e15f9104401becca02b
MindfulLearner <dimajosh1009@gmail.com>
2024-09-11 00:01:38 +0000

    love of coding

--- vite-project/src/components/Header.vue
+++ vite-project/src/components/Header.vue
@@ -9,15 +9,28 @@
 </script>
 
 <template>
-    <div>
+    <div class="ciao">
         <h1>
             {{ message }}
         </h1>
+        <div class="swagger">
+            <img src="/Expo85_sony.jpg" alt="">
+        </div>
     </div>
 </template>
 
 <style scoped>
     * {
         color: lightblue;
+    }
+
+    img {
+        width: 100%;
+        height: 100%;
+    }
+
+    .swagger {
+        width: 300px;
+        height: 100px;
     }
 </style>
